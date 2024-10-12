2063fedf290490e14b2cd78cb8ac9cf21634b2b7
palemoons <palemoons14@gmail.com>
2024-10-12 16:02:22 +0800

    adds spinner for comment loading

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useTheme } from "next-themes";
 import { SITE_CONFIG } from "@/app/site.config";
+import Spinner from "./Spinner";
 import styles from "./Comments.module.css";
 
 export default function Comments() {
@@ -9,21 +10,26 @@ export default function Comments() {
   return (
     <section className={styles.comments}>
       <div className={styles.title}>Comments</div>
-      <div
-        className={styles.container}
-        ref={(element) => {
-          if (!element) return;
+      <div className={styles.loading}>
+        <div className={styles.spinner}>
+          <Spinner />
+        </div>
+        <div
+          className={styles.container}
+          ref={(element) => {
+            if (!element) return;
 
-          const scriptElement = document.createElement("script");
-          scriptElement.setAttribute("src", "https://utteranc.es/client.js");
-          scriptElement.setAttribute("repo", SITE_CONFIG.commentRepo);
-          scriptElement.setAttribute("issue-term", "pathname");
-          scriptElement.setAttribute("theme", theme === "light" ? "github-light" : "github-dark");
-          scriptElement.setAttribute("crossorigin", "anonymous");
-          scriptElement.setAttribute("async", "true");
-          element.replaceChildren(scriptElement);
-        }}
-      />
+            const scriptElement = document.createElement("script");
+            scriptElement.setAttribute("src", "https://utteranc.es/client.js");
+            scriptElement.setAttribute("repo", SITE_CONFIG.commentRepo);
+            scriptElement.setAttribute("issue-term", "pathname");
+            scriptElement.setAttribute("theme", theme === "light" ? "github-light" : "github-dark");
+            scriptElement.setAttribute("crossorigin", "anonymous");
+            scriptElement.setAttribute("async", "true");
+            element.replaceChildren(scriptElement);
+          }}
+        />
+      </div>
     </section>
   );
 }
