d248b7c58fd0dfa495ef67790365868199260941
palemoons <palemoons14@gmail.com>
2024-10-21 23:30:33 +0800

    moves abbrlink generation to posts repository

--- scripts/handleStaticFiles.ts
+++ scripts/handleStaticFiles.ts
@@ -1,14 +1,13 @@
 import fs from "fs";
 import path from "path";
-import matter, { stringify } from "gray-matter";
+import matter from "gray-matter";
 import yaml from "js-yaml";
-import crc32 from "@/lib/crc32";
 import { encodeImgName } from "@/lib/images";
 import { IPostHeader } from "@/interfaces/Post";
 
 const postsDir = path.join(process.cwd(), "_posts");
 const publicImgDir = path.join(process.cwd(), "public", "img");
-const nonPostDirs = ["about"];
+const nonPostDirs = ["about", "scripts"];
 
 const customMatter = (content: string) =>
   matter(content, {
@@ -32,14 +31,8 @@ categories.forEach((category) => {
       const fullPath = path.join(postFolder, post, `${post}.md`);
       const fileContents = fs.readFileSync(fullPath, "utf8");
       // Use gray-matter to parse the post metadata section
-      const { data: frontMatter, content } = customMatter(fileContents);
-      // Create abbrlink if not exist
-      if (frontMatter.abbrlink == null) {
-        const { title, date } = frontMatter;
-        frontMatter.abbrlink = crc32(`${title}-${date}`);
-        console.log(`Creating new abbrlink: ${frontMatter.title} -> ${frontMatter.abbrlink}`);
-        fs.writeFileSync(fullPath, stringify(content, frontMatter));
-      }
+      const { data: frontMatter } = customMatter(fileContents);
+
       const postInfo: IPostHeader = {
         title: frontMatter.title,
         category: category,
@@ -49,16 +42,14 @@ categories.forEach((category) => {
       };
       postIndex.set(frontMatter.abbrlink, postInfo);
 
-      const year = new Date(frontMatter.date).getFullYear().toString();
       const files = fs.readdirSync(path.join(postFolder, post));
       files.forEach((file) => {
         const ext = path.extname(file);
         if ([".jpg", ".png", ".jpeg", ".webp", ".gif"].includes(ext)) {
-          const encodedFileName = encodeImgName(path.join(category, year, post), path.basename(file)) + ext;
+          const encodedFileName = encodeImgName(path.join(category, yearFolder, post), path.basename(file)) + ext;
           const originalFilePath = path.join(postFolder, post, file);
           const destinationPath = path.join(publicImgDir, encodedFileName);
           fs.copyFileSync(originalFilePath, destinationPath);
-          console.log(`Copy image: ${file} -> ${encodedFileName}`);
         }
       });
     });
