1625907558a5b58f163d4b6d4b66657e22d5f832
palemoons <palemoons14@gmail.com>
2024-12-30 20:54:15 +0800

    fixes bug of not found page title

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,18 +1,14 @@
 "use client";
-import { Metadata } from "next";
+
 import { SITE_CONFIG } from "./site.config";
 import styles from "./not-found.module.css";
 
 export default function NotFound() {
   return (
     <div className={`${styles.notfound} container`}>
+      <title>{`404 | ${SITE_CONFIG.title}`}</title>
       <div className={styles.title}>Oops!</div>
       <div>This page could not be found.</div>
     </div>
   );
 }
-
-export const metadata: Metadata = {
-  title: `404 | ${SITE_CONFIG.title}`,
-  description: "404 | Not Found",
-};
